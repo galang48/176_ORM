c5883a2c9461d4cac23ce1d97aaf490095531d23
galang48 <galangyudha1945@gmail.com>
2025-10-28 14:34:58 +0700

    Menambahkan middleware untuk parsing request dan menjalankan server

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const app = express();
 const db = require('./models');
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 
 
